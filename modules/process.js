81fc828b770b17835499019dc5e2970abcaf0f6a
JokerOrphelin49 <fralaufri@gmail.com>
2023-05-23 08:02:47 +0200

    essai js 6

--- modules/process.js
+++ modules/process.js
@@ -1,4 +1,4 @@
-import { createRoot } from 'react-dom/client';
+import { createRoot } from '../node_modules/react-dom/client';
 
 function NavigationBar() {
   // TODO: Actually implement a navigation bar
